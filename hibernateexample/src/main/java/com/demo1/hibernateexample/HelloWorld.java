89dea467da83402c19c6628af824d5d51feeea40
Saritha Pilli <Saritha Pilli@192.168.0.18>
2019-03-06 16:27:38 -0600

    trying for conflict

--- hibernateexample/src/main/java/com/demo1/hibernateexample/HelloWorld.java
+++ hibernateexample/src/main/java/com/demo1/hibernateexample/HelloWorld.java
@@ -3,6 +3,6 @@ package com.demo1.hibernateexample;
 public class HelloWorld {
 public static void main(String args)
 {
-	System.out.println("hai Saritha");
+	System.out.println("hello Ishaan");
 }
 }
