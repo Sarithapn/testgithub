f93ce17584a15105428bc3b7a183bc1ad918fa60
Saritha Pilli <Saritha Pilli@192.168.0.18>
2019-03-10 21:51:50 -0500

    added forlist

---
+++ hibernateexample/src/main/java/com/demo1/hibernateexample/ForList.java
@@ -0,0 +1,11 @@
+package com.demo1.hibernateexample;
+
+public class ForList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++)
+			System.out.println("hello "+i);
+	}
+
+}
