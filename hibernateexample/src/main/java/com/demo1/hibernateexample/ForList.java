46a34c325d207098da513636357f7a7c069c0197
Ishann <Saritha Pilli@192.168.0.18>
2019-03-10 22:08:14 -0500

    loop changed

--- hibernateexample/src/main/java/com/demo1/hibernateexample/ForList.java
+++ hibernateexample/src/main/java/com/demo1/hibernateexample/ForList.java
@@ -5,7 +5,7 @@ public class ForList {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=1;i<=5;i++)
-			System.out.println("hello "+i);
+			System.out.println("hello  ishaan "+i);
 	}
 
 }
